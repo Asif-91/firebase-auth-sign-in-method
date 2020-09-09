c55b7e185a3360fb2023f49315bbb9e96cfa00f0
Asif-91 <asifchowdhury323@gmail.com>
2020-09-09 22:03:59 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,123 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
+
+firebase.initializeApp(firebaseConfig);
 
 function App() {
+
+  const [user, setUser] = useState({
+    isSignedIn: false,
+    name: '',
+    emails: '',
+    password: '',
+    photo: ''
+  })
+  const provider = new firebase.auth.GoogleAuthProvider();
+  const handleSignIn = () => {
+    firebase.auth().signInWithPopup(provider)
+      .then(res => {
+        const { displayName, photoURL, email } = res.user;
+        const signedInUser = {
+          isSignedIn: true,
+          name: displayName,
+          email: email,
+          photo: photoURL,
+        }
+        setUser(signedInUser);
+        console.log(displayName, email, photoURL);
+      })
+      .catch(err => {
+        console.log(err);
+        console.log(err.message)
+      })
+  }
+
+  const handleSignOut = () => {
+    firebase.auth().signOut()
+      .then(res => {
+        const signedOutUser = {
+          isSignedIn: false,
+          name: '',
+          photo: '',
+          email: '',
+          error: '',
+          success: false
+        }
+        setUser(signedOutUser);
+        console.log(res);
+      })
+      .catch(err => {
+
+      })
+  }
+  const handleBlur = (e) => {
+    let isFieldValid = true;
+    if (e.target.name === 'email') {
+      isFieldValid = /\S+@\S+\.\S+/.test(e.target.value);
+    }
+    if (e.target.name === 'password') {
+      const isPasswordValid = e.target.value.length > 6;
+      const passwordHasNumber = /\d{1}/.test(e.target.value);
+      isFieldValid = isPasswordValid && passwordHasNumber;
+    }
+    if (isFieldValid) {
+      //[...cart, newItem]
+      const newUserInfo = { ...user };
+      newUserInfo[e.target.name] = e.target.value;
+      setUser(newUserInfo);
+    }
+  }
+
+  const handleSubmit = (e) => {
+    // console.log(user.email, user.password)
+    if(user.email && user.password){
+      firebase.auth().createUserWithEmailAndPassword(user.mail, user.password)
+      .then (res => {
+        const newUserInfo = { ...user };
+        newUserInfo.error = '';
+        newUserInfo.success = true;
+        setUser(newUserInfo);
+      })
+      .catch( error => {
+        const newUserInfo ={...user};
+        newUserInfo.error = error.message;
+        newUserInfo.success = false;
+        setUser(newUserInfo);
+      });
+    }
+    e.preventDefault();
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Testing</h1>
+      {
+        user.isSignedIn ? <button onClick={handleSignOut}>Sing out</button> :
+          <button onClick={handleSignIn}>Sing in</button>
+      }
+
+      {
+        user.isSignedIn && <div>
+          <p>Welcome, {user.name}</p>
+          <p>Your email:  {user.email}</p>
+          <img src={user.photo} alt=""></img>
+        </div>
+      }
+      <h1>Our Own Authentication</h1>
+      
+      <form onSubmit={handleSubmit}>
+        <input name="name" type="text" onBlur={handleBlur} placeholder="Your name"/>
+        <br/>
+        <input type="text" onBlur={handleBlur} name="email" placeholder="Your Email address" required />
+        <br />
+        <input type="password" onBlur={handleBlur} name="password" placeholder="Your Password" required />
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
+      <p style={{color: 'red'}}>{user.error}</p>
+      { user.success && <p style={{color: 'green'}}>User Created Successfully</p>}
     </div>
   );
 }
---
+++ src/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC0_P3dOiu4vRP_JIMEkUl1Ve-nDqEkwIQ",
+    authDomain: "fir-auth-6a709.firebaseapp.com",
+    databaseURL: "https://fir-auth-6a709.firebaseio.com",
+    projectId: "fir-auth-6a709",
+    storageBucket: "fir-auth-6a709.appspot.com",
+    messagingSenderId: "894891439894",
+    appId: "1:894891439894:web:937a71a6df28dd23030237"
+  };
+
+export default firebaseConfig;  
